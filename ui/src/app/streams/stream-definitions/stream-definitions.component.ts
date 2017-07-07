6fdffbdecd2242ccfed71519826bb57421a6906f
Janne Valkealahti <janne.valkealahti@gmail.com>
2017-07-07 16:45:27 +0100

    stash

--- ui/src/app/streams/stream-definitions/stream-definitions.component.ts
+++ ui/src/app/streams/stream-definitions/stream-definitions.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Page } from '../../shared/model/page';
+import { StreamDefinition } from '../model/stream-definition';
+import { StreamsService } from '../streams.service';
+import { Observable } from 'rxjs/Observable';
+
 
 @Component({
   selector: 'app-stream-definitions',
@@ -6,9 +11,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StreamDefinitionsComponent implements OnInit {
 
-  constructor() { }
+  streamDefinitions: Page<StreamDefinition>;
+
+  constructor(
+    public streamsService: StreamsService) {
+  }
+  
+  public items: Observable<Array<any>>;
+  private _items: Array<any>;
 
   ngOnInit() {
+    this._items = [];
+    this.items = Observable.of(this._items);
+    console.log('hello');
+    this.streamsService.getDefinitions().subscribe(
+      data => {
+        console.log('DATA', data);
+//        for (let i of data.items) {
+//          this._items.push(i);
+//        }
+        this.streamDefinitions = data;        
+      }
+    );
   }
 
 }
--- ui/src/app/streams/streams.service.ts
+++ ui/src/app/streams/streams.service.ts
@@ -1,26 +1,46 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
+import { Page } from '../shared/model/page';
+import { StreamDefinition } from './model/stream-definition';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class StreamsService {
 
-  private appstUrl = '/apps';
+  public streamDefinitions: Page<StreamDefinition>;
+
+  private streamDefinitionsUrl = '/streams/definitions';
 
   constructor(private http: Http) { }
 
-  getApps(): Observable<any[]> {
-    return this.http.get(this.appstUrl)
-                    .map(this.extractData)
+  getDefinitions(): Observable<Page<StreamDefinition>> {
+    return this.http.get(this.streamDefinitionsUrl)
+                    .map(this.extractData.bind(this))
                     .catch(this.handleError);
   }
 
-  private extractData(res: Response) {
+  private extractData(res: Response) : Page<StreamDefinition> {
     const body = res.json();
-    return body._embedded;
+    let items: StreamDefinition[];
+    if (body._embedded && body._embedded.streamDefinitionResourceList) {
+      items = body._embedded.streamDefinitionResourceList as StreamDefinition[];
+    }
+    else {
+      items = [];
+    }
+
+    let page = new Page<StreamDefinition>();
+    page.items = items;
+    page.totalElements = items.length;
+
+    this.streamDefinitions = page;
+
+    console.log('Extracted Stream Definitions:', this.streamDefinitions);
+    return page;
   }
+
 
   private handleError (error: Response | any) {
     // In a real world app, you might use a remote logging infrastructure
