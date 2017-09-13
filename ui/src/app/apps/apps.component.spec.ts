be35287c8c6edc56c2f9d1cbaf749514cc8f3fc7
Janne Valkealahti <janne.valkealahti@gmail.com>
2017-09-13 15:36:31 +0100

    stash

--- ui/src/app/apps/apps.component.spec.ts
+++ ui/src/app/apps/apps.component.spec.ts
@@ -1,14 +1,49 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { NgxPaginationModule } from 'ngx-pagination';
+import { BusyModule } from 'tixif-ngx-busy';
+import {ModalDirective, ModalModule, PopoverModule} from 'ngx-bootstrap';
+import { ToastyService } from 'ng2-toasty';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { RolesDirective } from '../auth/directives/roles.directive';
 import { AppsComponent } from './apps.component';
+import { MockToastyService } from '../tests/mocks/toasty';
+import { AppsService } from './apps.service';
+import { TriStateButtonComponent } from '../shared/components/tri-state-button.component';
+import { TriStateCheckboxComponent } from '../shared/components/tri-state-checkbox.component';
+import { MockAuthService } from '../tests/mocks/auth';
+import { AuthService } from '../auth/auth.service';
+import { MockAppsService } from '../tests/mocks/apps';
 
-xdescribe('AppsComponent', () => {
+describe('AppsComponent', () => {
   let component: AppsComponent;
   let fixture: ComponentFixture<AppsComponent>;
+  const toastyService = new MockToastyService();
+  const authService = new MockAuthService();
 
   beforeEach(async(() => {
+    const appsService = new MockAppsService();
     TestBed.configureTestingModule({
-      declarations: [ AppsComponent ]
+      declarations: [
+        TriStateButtonComponent,
+        TriStateCheckboxComponent,
+        RolesDirective,
+        AppsComponent
+      ],
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        BusyModule,
+        NgxPaginationModule,
+        ModalModule.forRoot(),
+        // PopoverModule.forRoot(),
+        RouterTestingModule.withRoutes([])
+      ],
+      providers: [
+        { provide: AuthService, useValue: authService },
+        { provide: AppsService, useValue: appsService },
+        { provide: ToastyService, useValue: toastyService }
+      ]
     })
     .compileComponents();
   }));
@@ -16,10 +51,10 @@ xdescribe('AppsComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(AppsComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should be created', () => {
+    fixture.detectChanges();
     expect(component).toBeTruthy();
   });
 });
---
+++ ui/src/app/tests/mocks/apps.ts
@@ -0,0 +1,21 @@
+import {AppRegistration} from "../../shared/model/app-registration.model";
+import {Page} from "../../shared/model/page";
+import {Observable} from "rxjs/Observable";
+
+export class MockAppsService {
+
+  // getApps(reload?: boolean, search?: string): Observable<Page<AppRegistration>> {
+  //
+  //   const page = new Page<AppRegistration>();
+  //
+  //   let items: AppRegistration[] = [];
+  //   page.items = items;
+  //   page.totalElements = 10;
+  //   page.totalPages = 1;
+  //   page.pageNumber = 0;
+  //   page.pageSize = 10;
+  //
+  //   console.log('XXXXXXX');
+  //   return Observable.of(page);
+  // }
+}
